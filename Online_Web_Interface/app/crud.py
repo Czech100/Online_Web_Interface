9ee5d1fbd6b17a585ce519ac0c3741d704092936
Czech100 <50641600+Czech100@users.noreply.github.com>
2023-04-12 19:57:06 -0400

    Add files via upload

---
+++ Online_Web_Interface/app/crud.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def create_project(db: Session, project: schemas.ProjectCreate):
+    db_project = models.Project(title=project.title, author=project.author, project_type=project.project_type, file_path = project.file_path)
+    db.add(db_project)
+    db.commit()
+    db.refresh(db_project)
+    return db_project
